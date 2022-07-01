768cdc6979bfe3186e24fd12579dd9fd48d3eaac
dhruvindsd-dev <dhruvinddev@gmail.com>
2022-07-01 11:26:15 +0530

    - updated server url

--- constants.ts
+++ constants.ts
@@ -5,15 +5,18 @@ export const EMAIL_REGEX = /^((?!\.)[\w\-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$/;
 export const C_EASE_OUT = [0.6, -0.01, 0, 1];
 export const DASHBOARD_CACHE = new Map();
 
-// export const SERVER_URL = "http://127.0.0.1:8000";
-export const SERVER_URL = "http://165.22.214.53:8000";
+export const SERVER_URL = "http://127.0.0.1:8000";
+// export const SERVER_URL = "http://165.22.214.53:8000";
 
 export const IS_CLIENT = typeof window !== "undefined";
+
 export const AXIOS_INSTANCE = axios.create({
 	baseURL: SERVER_URL,
 	timeout: 5000,
 });
+
 const token = IS_CLIENT && localStorage.getItem("token");
+
 AXIOS_INSTANCE.interceptors.request.use((req) => {
 	if (token) req.headers!.Authorization = `Token ${token}`;
 	return req;
